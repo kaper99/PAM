01ed53ca67fdfd0ec7619d593201999ec03be75f
studen <example@mail.com>
2021-10-26 11:39:24 +0200

    Lab3

--- app/src/main/java/com/example/myfrag/Fragment3.java
+++ app/src/main/java/com/example/myfrag/Fragment3.java
@@ -3,10 +3,14 @@ package com.example.myfrag;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Fragment3 extends Fragment {
+
+    private TextView text;
+    private Button button;
+    private FragsData fragsData;
+    private Observer<Integer> numberObserver;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,16 +58,31 @@ public class Fragment3 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+        text = (TextView) view.findViewById(R.id.current);
+        button = (Button) view.findViewById(R.id.button_decrease);
+        fragsData = new ViewModelProvider(requireActivity()).get(FragsData.class);
+        numberObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer newInteger) {
+                text.setText(newInteger.toString());
+            }
+        };
+        fragsData.counter.observe(getViewLifecycleOwner(), numberObserver);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                                          Integer i = fragsData.counter.getValue();
+                                          fragsData.counter.setValue(--i);
+                                      }
+                                  }
+        );
+        return view;
     }
 }
--- app/src/main/java/com/example/myfrag/Fragment4.java
+++ app/src/main/java/com/example/myfrag/Fragment4.java
@@ -3,10 +3,15 @@ package com.example.myfrag;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Fragment4 extends Fragment {
+
+    private FragsData fragsData;
+    private Observer<Integer> numberObserver;
+
+    private EditText edit;
+    private TextWatcher textWatcher;
+    private boolean turnOffWatcher;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +71,56 @@ public class Fragment4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_4, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_4, container, false);
+
+        //1.
+        edit = view.findViewById(R.id.editTextNumber);
+
+        //2.
+        fragsData = new ViewModelProvider(requireActivity()).get(FragsData.class);
+
+        //3.
+        numberObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer newInteger) {
+                turnOffWatcher = true;
+                edit.setText(newInteger.toString());
+                edit.setSelection(edit.length());
+            }
+        };
+        //4.
+        fragsData.counter.observe(getViewLifecycleOwner(), numberObserver);
+        //5.
+        textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!turnOffWatcher) {
+                    Integer i;
+                    try {
+                        i = Integer.parseInt(s.toString());
+                    } catch (NumberFormatException e) {
+                        i = fragsData.counter.getValue();
+                    }
+                    fragsData.counter.setValue(i);
+
+
+                } else {
+                    turnOffWatcher = !turnOffWatcher;
+                }
+            }
+        };
+        //6.
+        edit.addTextChangedListener(textWatcher);
+
+        return view;
     }
 }
